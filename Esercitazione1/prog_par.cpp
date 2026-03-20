313e1169f2b4c5eb6e18f646d73b18a5f57d3975
simoneferretti <simonef0207@gmail.com>
2026-03-20 15:17:13 +0000

    esercitazione 2 svolta

--- Esercitazione1/prog_par.cpp
+++ Esercitazione1/prog_par.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, const char *argv[])
-{
+{	
 	std::cout << "Numero di parametri: " << argc << "\n";
 	for (int i = 0; i < argc; i++) {
 		std::cout << "Parametro " << i << " = " << argv[i] << "\n";
